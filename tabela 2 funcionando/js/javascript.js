db28c146e68f83203ebaf409f5a3df91b0bd9e0e
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-02-27 13:31:39 -0300

    ok2

--- tabela 2 funcionando/js/javascript.js
+++ tabela 2 funcionando/js/javascript.js
@@ -4,179 +4,40 @@ var troca=0;
 var dele=0;
 var naorepete=0;
 
-function salvar (){
+function salvar (){//SALVA OS DADOS ESCRITOS NO MODAL
 	Nome =  $('#nome').val();
 	console.log(Nome);
 	Valor = $('#valor').val();
 	Estoque = $('#estoque').val();
 	Status = $('#status').val();
 }
-$(document).ready(function(){
-	$("#alertconcluido").hide();
-	$("#alerteditado").hide();
-	tudo();
-	$("#estoque").keypress(verificaNumeroEstoque);
-	$("#nome").keypress(Bloqueianumeros);
-	$("#valor").keypress(verificaNumerosValor);
 
-	$("#loga").click(function(){
-		Login();
-	});
 
-	$("#valor").maskMoney({decimal:".", thousands:"", million:"."
-	});
-
-	$("#deletinho").click(function(){
-		del();
-	});
-
-    $("#inativos").click(function(){
-    	troca = 400;
-    	tudo();
-	});
-
-    $("#ativos").click(function(){
-    	troca = 0;
-		tudo();
-	});
-
-     $("#myBtn").click(function(){
-    	alteranome = 0;
-    	titulo();
-	});
-	
-	function del(){
-		$.ajax({
-		type: 'DELETE',
-		url: server+ dele,
-		success: tudo
-		});
-		$('#myModal').modal('hide');
-	
-	}
-
-	$('#confirmar').click(function(){
-		salvar();
-		if(Nome=="" || Valor=="" || Estoque==""){
-			$("#alertcampovazio").show();
-			window.setTimeout(function() {
-    		$(".alert").slideUp(500, function(){
-        	$(this).hide(); 
-    		});
-			}, 2000);
-		}else{
-			for(var i=0;i<dados.length;i++){
-				if(dados[i].nome.toLowerCase()===Nome.toLowerCase()){naorepete=1;}
-			}
-			nomeIgual();
-    	}
-    });
-
-    nomeIgual = function(){
-    		if(naorepete==0){
-		        $.ajax({
-		            url : server,
-		            type : 'POST',
-		            data : {
-		            	nome: Nome,
-		            	valor: Valor,
-		            	estoque: Estoque,
-		            	status: Status
-		            },
-						success: tudo
-		            
-		        });
-		        $('#myModal').modal('hide');
-		        $("#alertconcluido").show();
-			window.setTimeout(function() {
-    		$(".alert").slideUp(500, function(){
-        	$(this).hide(); 
-    		});
-			}, 2000);
-
-			}else{
-
-			$("#alertnaorepete").show();
-			window.setTimeout(function() {
-    		$(".alert").slideUp(500, function(){
-        	$(this).hide(); 
-    		});
-			}, 2000);
-			naorepete=0;
-			}
-    }
-
-	$('#editaron').click(function(){
-        salvar();
-        if(Nome=="" || Valor=="" || Estoque==""){
-        	$("#alertcampovazio").show();
-			window.setTimeout(function() {
-    		$(".alert").slideUp(500, function(){
-        	$(this).hide(); 
-    		});
-			}, 2000);
-		}else{
-        $.ajax({
-            url : server+ muda,
-            type : 'PUT',
-            data : {
-            	nome: Nome,
-            	valor: Valor,
-            	estoque: Estoque,
-            	status: Status
-            },
-				success: tudo
-        });
-        $('#myModal').modal('hide');
-        $("#alerteditado").show();
-			window.setTimeout(function() {
-    		$(".alert").slideUp(500, function(){
-        	$(this).hide(); 
-    		});
-			}, 2000);
-       }
-    });
-    
-	$("#myBtn").click(function(){
-		$("#alertcampovazio").hide();
-		$("#alertnaorepete").hide();
-        $("#myModal").modal();
-        $("#editaron").hide();
-		$("#deletinho").hide();
-		$("#confirmar").show();
-		$("#cancela").show();
-		$("#textboxs").show();
-		limparcampo();
-
-    });
-
-});
-
-function tudo(){
+function tudo(){//PRINTA E ARRUMA O VALOR
 	$('#tabelalinda').empty(); //Limpando a tabela
-    $.get(server, function(data) {
-        dados=data;
-        for(var i=0;i<dados.length;i++){
-        	j= dados[i].id;
-        	var n = dados[i].valor.toString();
-        	n = parseFloat(n).toFixed(2)
-        	n = n.replace(".", ",");
-        	if (troca==0) {
-        		if(dados[i].status == "A"){
-        		$('#tabelalinda').append('<tr><td>'+dados[i].id+'</td><td>'+dados[i].nome+'</td><td>'+'R$ '+n+' '+'</td><td>'+'<span class="glyphicon glyphicon-thumbs-up"></span>'+'</td><td>'+dados[i].estoque+'</td><td>'+'<button type="button" onclick= "editar('+i+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+ '</td><td>'+'<button type="button" onclick= "deleta('+j+') " data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>' +'</td></tr>');
-        	}  
-        	
-        	}
-        	else{
-        		if(dados[i].status == "I"){
-        			$('#tabelalinda').append('<tr><td>'+dados[i].id+'</td><td>'+dados[i].nome+'</td><td>'+'R$ '+n+' '+'</td><td>'+'<span class="glyphicon glyphicon-thumbs-down"></span>'+'</td><td>'+dados[i].estoque+'</td><td>'+'<button type="button" onclick= "editar('+i+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+ '</td><td>'+'<button type="button"  onclick= "deleta('+j+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>' +'</td></tr>');
-        		}
-        	}  
-        }
-    });
+	$.get(server, function(data) {
+		dados=data;
+		for(var i=0;i<dados.length;i++){
+			j= dados[i].id;
+			var n = dados[i].valor.toString();
+			n = parseFloat(n).toFixed(2)
+			n = n.replace(".", ",");
+			if (troca==0) {
+				if(dados[i].status == "A"){
+				$('#tabelalinda').append('<tr><td>'+dados[i].id+'</td><td>'+dados[i].nome+'</td><td>'+'R$ '+n+' '+'</td><td>'+'<span class="glyphicon glyphicon-thumbs-up"></span>'+'</td><td>'+dados[i].estoque+'</td><td>'+'<button type="button" onclick= "editar('+i+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+ '</td><td>'+'<button type="button" onclick= "deleta('+j+') " data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>' +'</td></tr>');
+			}  
+			
+			}
+			else{
+				if(dados[i].status == "I"){
+					$('#tabelalinda').append('<tr><td>'+dados[i].id+'</td><td>'+dados[i].nome+'</td><td>'+'R$ '+n+' '+'</td><td>'+'<span class="glyphicon glyphicon-thumbs-down"></span>'+'</td><td>'+dados[i].estoque+'</td><td>'+'<button type="button" onclick= "editar('+i+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>'+ '</td><td>'+'<button type="button"  onclick= "deleta('+j+')" data-toggle="modal" data-target="#myModal" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>' +'</td></tr>');
+				}
+			}  
+		}
+	});
 }
 
-function Login(){
+function Login(){//IDENTIFICAÇÃO DE LOGIN
 var done=0;
 var username=document.login.username.value;
 username=username.toLowerCase();
@@ -189,8 +50,8 @@ if (done==0) { alert("Senha ou Usuário inválido."); }
 }
 
 
-deleta = function (v){
-	dele = v;
+function deleta(v){//FUNÇÃO QUE É CHAMADA 
+	dele = v; //ID DO PRODUTO SALVO AQUI
 	$("#confirmar").hide();
 	$("#editaron").hide();
 	$("#deletinho").show();
@@ -203,8 +64,16 @@ deleta = function (v){
 	titulo();
 	}
 
+function del(){
+	$.ajax({
+	type: 'DELETE',
+	url: server+ dele,
+	success: tudo //SE TIVER SUCESSO, VAI LIMPAR A TABELA E PRINTAR NOVAMENTE
+	});
+	$('#myModal').modal('hide');
+}
 
-editar = function(o){
+ function editar(o){//FUNÇÃO PARA EDITAR
 	$("#confirmar").hide();
 	$("#editaron").show();
 	$("#deletinho").hide();
@@ -214,21 +83,21 @@ editar = function(o){
 	$("#textboxs").show();
 	alteranome = 1;
 	titulo();
-    document.getElementById('nome').value = dados[o].nome;
-    document.getElementById('valor').value = dados[o].valor;
-    document.getElementById('status').value = dados[o].status;
-    document.getElementById('estoque').value = dados[o].estoque;
-    muda = dados[o].id;
+	document.getElementById('nome').value = dados[o].nome;//PEGA AS INFORMAÇÕES EXISTENTES
+	document.getElementById('valor').value = dados[o].valor;
+	document.getElementById('status').value = dados[o].status;
+	document.getElementById('estoque').value = dados[o].estoque;
+	muda = dados[o].id;
 }
 
-function limparcampo(){
+function limparcampo(){//O MODAL INICIA VAZIO
 	$('#nome').val("");
 	$('#valor').val("");
 	$('#estoque').val("");
  
 	}
 
-function titulo(){
+function titulo(){//SELECIONA O TITULO A PARTIR DO VALOR
 	if(alteranome==0){
 		 document.getElementById('titulo').innerHTML = "  Adicionar Itens";
 	}else if(alteranome==1){
@@ -238,20 +107,148 @@ function titulo(){
 	}
 }
 
-function verificaNumeroEstoque(e) {
-    if ((e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) || e.which ==32 )   {
-         return false;
-    }
+function verificaNumeroEstoque(e){ //BLOQUEIA LETRAS NO ESTOQUE
+	if ((e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) || e.which ==32 )   {
+		 return false;
+	}
 }
 
-function verificaNumerosValor(e){
+function verificaNumerosValor(e){ //BLOQUEIA LETRAS NO VALOR
 	if ((e.which != 8 && e.which != 0 && (e.which < 46 || e.which > 57)) || e.which ==47) {
-         return false;
-    }
+		 return false;
+	}
 }
 
-function Bloqueianumeros(e){
-	if ((e.which != 8 && e.which != 0 && (e.which < 94 || e.which > 126)) || (e.which>=123 && e.which<=125) || e.which==95) {
-         return false;
-    }
+function Bloqueianumeros(e){ //BLOQUEIA NUMEROS NO NOME
+	if ((e.which != 8 && e.which != 0 && (e.which < 65 || e.which > 126)) || (e.which>=123 && e.which<=125) || e.which==20 ) {
+		 return false;
+	}
 }
+
+function nomeIgual(){
+		if(naorepete==0){
+			$.ajax({
+				url : server,
+				type : 'POST',
+				data : {
+					nome: Nome,
+					valor: Valor,
+					estoque: Estoque,
+					status: Status
+				},
+				success: tudo	
+			});
+			$('#myModal').modal('hide');//FECHAMENTO DE MODAL
+			$("#alertconcluido").show();//ALERTA DE CONCLUIDO
+			window.setTimeout(function() {
+			$(".alert").slideUp(500, function(){
+			$(this).hide(); 
+			});
+			}, 2000);
+
+		}else{
+			$("#alertnaorepete").show();//ALERTA DE REPETIÇÃO
+			window.setTimeout(function() {
+			$(".alert").slideUp(500, function(){
+			$(this).hide(); 
+			});
+			}, 2000);
+			naorepete=0;
+		}
+}
+
+function actions(){//FUNÇÕES DE BOTÕES E INPUT
+	$("#alertconcluido").hide();
+	$("#alerteditado").hide();
+	$("#estoque").keypress(verificaNumeroEstoque);
+	$("#nome").keypress(Bloqueianumeros);
+	$("#valor").keypress(verificaNumerosValor);
+
+	$("#loga").click(function(){
+		Login();
+	});
+	$("#valor").maskMoney({decimal:".", thousands:"", million:"." //MASCARA DE DINHEIRO
+	});
+
+	$("#deletinho").click(function(){ //DELETA OS DADOS DEFINITIVAMENTE
+		del();
+	});
+
+	$("#inativos").click(function(){//ABRE ITENS INATIVOS
+		troca = 400;
+		tudo();
+	});
+
+	$("#ativos").click(function(){//ABRE ITENS ATIVOS
+		troca = 0;
+		tudo();
+	});
+
+	$("#myBtn").click(function(){ //ABRE O MODAL
+		alteranome = 0;
+		titulo();
+	});
+	$('#confirmar').click(function(){
+		salvar();//PUXA DADOS DIGITADOS NO MODAL
+		if(Nome=="" || Valor=="" || Estoque==""){//SE ESTIVER ALGUM CAMPO VAZIO DA O ALERTA E BLOQUEIA O SUBMIT
+			$("#alertcampovazio").show();
+			window.setTimeout(function() {
+			$(".alert").slideUp(500, function(){
+			$(this).hide(); 
+			});
+			}, 2000);
+		}else{
+			for(var i=0;i<dados.length;i++){//FAZ VARREDURA PARA VERIFICAR EXISTENCIA DE NOME IGUAL
+				if(dados[i].nome.toLowerCase()===Nome.toLowerCase()){naorepete=1;}//ACHANDO NOME IGUAL, VARIAVEL SETA PARA 1
+			}
+			nomeIgual();//CHAMA FUNÇÃO QUE VERIFICA NOME IGUAL
+		}
+	});
+	$('#editaron').click(function(){
+		salvar();
+		if(Nome=="" || Valor=="" || Estoque==""){// ALERTA DE CAMPO VAZIO
+			$("#alertcampovazio").show();
+			window.setTimeout(function() {
+			$(".alert").slideUp(500, function(){
+			$(this).hide(); 
+			});
+			}, 2000);
+		}else{ //EDITA O ITEM SELECIONADO
+		$.ajax({
+			url : server+ muda,
+			type : 'PUT',
+			data : {
+				nome: Nome,
+				valor: Valor,
+				estoque: Estoque,
+				status: Status
+			},
+				success: tudo
+		});
+		$('#myModal').modal('hide');//FECHA MODAL
+		$("#alerteditado").show();//MENSAGEM DE EDIÇÃO CONCLUIDA
+			window.setTimeout(function() {
+			$(".alert").slideUp(500, function(){
+			$(this).hide(); 
+			});
+			}, 2000);
+	   }
+	});
+	$("#myBtn").click(function(){ //CHAMA O MODAL INICIAL DE ADICIONAR ITEM
+		$("#alertcampovazio").hide();
+		$("#alertnaorepete").hide();
+		$("#myModal").modal();
+		$("#editaron").hide();
+		$("#deletinho").hide();
+		$("#confirmar").show();
+		$("#cancela").show();
+		$("#textboxs").show();
+		limparcampo();//CHAMA O MODAL VAZIO
+
+	});
+}
+
+$(document).ready(function(){//O QUE DEVE SER EXECUTADO AO INICIAR A PAGINA
+	tudo();
+	actions();
+});
